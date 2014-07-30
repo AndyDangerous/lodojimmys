4cc1fd60f745eaac253a5b5451a52bd0b12c0fb2
Andy Mention <amention@gmail.com>
2014-07-29 19:23:54 -0600

    Fix the most obnoxious /lodojimmys/lodojimmys issue of all time/the last hour

--- business_logic/menu_db.rb
+++ business_logic/menu_db.rb
@@ -5,7 +5,7 @@ class MenuDB
   attr_accessor :database
 
   def initialize
-    db ||= Sequel.sqlite("/Users/andy/projects/sinatra/lodojimmys/db/menu.db")
+    db ||= Sequel.sqlite("#{File.expand_path('./db')}/menu.db")
     @database ||= db[:jimmys_db]
   end
 
@@ -39,29 +39,3 @@ class MenuDB
     database.insert(data)
   end
 end
-
-
-#     data = CSV.read('/Users/andy/projects/sinatra/lodojimmys/db/jimmys_menu.csv', headers: true, header_converters: :symbol)
-#     rows = data.each do |row|
-#       database.insert(:id => row[:id],
-#                       :menu        => row[:menu],
-#                       :category    => row[:category],
-#                       :name        => row[:name],
-#                       :description => row[:description],
-#                       :price       => row[:price]
-#       )
-#     end
-#
-#     def self.create
-#       DB = Sequel.sqlite("/Users/andy/projects/sinatra/lodojimmys/db/menu.db")
-#
-#       DB.create_table :jimmys_db do
-#         primary_key :id
-#         String :menu
-#         String :category
-#         String :name
-#         String :description
-#         String :price
-#       end
-#     end
-# end
